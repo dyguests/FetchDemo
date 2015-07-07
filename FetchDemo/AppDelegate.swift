85a8f679c9d6716b03eb328d2386cbdd7d94b477
fanhl <Dyguest0315>
2015-07-07 14:10:20 +0800

    init
    a demo for iOS Background Fetch

--- FetchDemo/AppDelegate.swift
+++ FetchDemo/AppDelegate.swift
@@ -13,34 +13,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        application.setMinimumBackgroundFetchInterval(UIApplicationBackgroundFetchIntervalMinimum)
+        
         return true
     }
+    
+    func application(application: UIApplication, performFetchWithCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
+        
+        var fetchStart:NSDate = NSDate()
+        
+        let viewController:ViewController = self.window?.rootViewController as! ViewController
+        viewController.fetchNewDataWithCompletionHandler { (result:UIBackgroundFetchResult) -> Void in
+            completionHandler(result)
 
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+            application.applicationIconBadgeNumber = 0
+
+            //显示执行时间
+            var fetchEnd:NSDate = NSDate()
+            var timeElapsed:NSTimeInterval = fetchEnd.timeIntervalSinceDate(fetchStart)
+            
+            NSLog("Background Fetch Duration: %f seconds", timeElapsed)
+        }
     }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
--- FetchDemo/ViewController.swift
+++ FetchDemo/ViewController.swift
@@ -8,18 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var dates:[String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //init tableView
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        //init data
+        dates.append("\(NSDate())")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //#UITableViewDataSource
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return dates.count
     }
-
-
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        var cell = tableView.dequeueReusableCellWithIdentifier("cell") as! UITableViewCell
+        
+        let item = dates[indexPath.row]
+        
+        let dateLbl = cell.viewWithTag(101) as! UILabel
+        
+        dateLbl.text = item
+        
+        return cell
+    }
+    
+    //#Fetch
+    
+    func fetchNewDataWithCompletionHandler(completionHandler:(UIBackgroundFetchResult)->Void){
+        NSLog("fetchNewDataWithCompletionHandler at \(NSDate())")
+        
+        self.dates.append("\(NSDate())")
+        self.tableView.reloadData()
+        
+        completionHandler(UIBackgroundFetchResult.NewData)
+    }
 }
 
